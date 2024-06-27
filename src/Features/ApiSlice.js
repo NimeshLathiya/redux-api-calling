0c300086afe7c720e02cf580040ffe41a087f8fb
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-06-27 07:37:48 +0530

    Update ApiSlice.js

--- src/Features/ApiSlice.js
+++ src/Features/ApiSlice.js
@@ -1,4 +1,4 @@
-import { , createSlice } from "@reduxjs/toolkit";
+import {createAsyncThunk , createSlice } from "@reduxjs/toolkit";
 
 // Define the async thunk
 export const getAllData = createAsyncThunk(
